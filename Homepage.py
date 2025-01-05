56bd852e437020395a9505169989cdb7fa5d08c0
vilanovaandrew <vilanovapandrew@outlook.com.br>
2025-01-05 12:56:32 -0300

    Add files via upload

--- Homepage.py
+++ Homepage.py
@@ -6,9 +6,9 @@ from mplsoccer.pitch import Pitch
 from mplsoccer import Pitch, VerticalPitch
 
 st.set_page_config(
-    page_title="Multiple Apps",
-    page_icon="🤜🤛",
+    page_title="Campeonato Maranhense Estatísticas",
+    page_icon="📊",
 )
 
-st.title("Main Page")
-st.sidebar.success("Select a page above.")
+st.title("Local das estatísticas do campeonato Maranhense 2025")
+st.sidebar.success("Seleciona páginas acima.")
